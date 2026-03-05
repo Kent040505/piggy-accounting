1d9705d1092f3aaba03f68cd5b1c552dcb864950
Kent0505 <148691474+Kent040505@users.noreply.github.com>
2026-03-06 03:41:17 +0800

    Create src/index.js with React DOM rendering code

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assuming you have an App component
+
+ReactDOM.render(<App />, document.getElementById('root'));
